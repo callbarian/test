525ea84c4bcbc6ac502482e98cd8db309c4faed9
callbarian <21100492@handong.edu>
2020-07-07 20:14:01 +0900

    seongmin

---
+++ etc/conda/activate.d/env_vars.sh
@@ -0,0 +1,2 @@
+#export LD_LIBRARY_PATH=/home/callbarian/bin/miniconda3/envs/c3d_py36/lib:${LD_LIBRARY_PATH}
+export LD_LIBRARY_PATH=:/usr/local/cuda-10.0/lib64:/home/callbarian/bin/miniconda3/envs/c3d_py36/lib/
---
+++ etc/conda/deactivate.d/env_vars.sh
@@ -0,0 +1,3 @@
+export LD_LIBRARY_PATH=/usr/lib
+unset LD_LIBRARY_PATH
+#export LD_LIBRARY_PATH=/usr/local/cuda-10.0/lib64 
